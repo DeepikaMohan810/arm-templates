cef194c7ca27b931aa86ab349543400d811b1d39
Deepika <deepika.m@8kmiles.com>
2017-12-20 11:32:02 +0530

    templateuri changed

--- GitTemplateDeploy.ps1
+++ GitTemplateDeploy.ps1
@@ -1,5 +1,5 @@
 ﻿New-AzureRmResourceGroup -Name DeepikaTestResourceGroup -Location "Southeast Asia"
-New-AzureRmResourceGroupDeployment -Name DeepikaTestResourceGroupDeployment -ResourceGroupName DeepikaTestResourceGroup -TemplateUri https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/GitTemplateDeploy.ps1 
+New-AzureRmResourceGroupDeployment -Name DeepikaTestResourceGroupDeployment -ResourceGroupName DeepikaTestResourceGroup -TemplateUri "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/GitTemplateDeploy.ps1" 
 
 
 
