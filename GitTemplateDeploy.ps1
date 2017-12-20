03708f33322e5073ae4aaf7aa8a4b7725b916e40
Deepika <deepika.m@8kmiles.com>
2017-12-20 15:30:40 +0530

    gitps

--- GitTemplateDeploy.ps1
+++ GitTemplateDeploy.ps1
@@ -1,5 +1,5 @@
 ﻿New-AzureRmResourceGroup -Name DeepikaTestResourceGroup -Location "Southeast Asia" -Force
-New-AzureRmResourceGroupDeployment -Name DeepikaTestResourceGroupDeployment -ResourceGroupName DeepikaTestResourceGroup -TemplateUri "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/GitTemplateDeploy.ps1" -Force 
+New-AzureRmResourceGroupDeployment -Name DeepikaTestResourceGroupDeployment -ResourceGroupName DeepikaTestResourceGroup -TemplateUri "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/nic.json"
 
 
 
