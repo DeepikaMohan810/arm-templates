26791538c9365f500f03a7370b9bdd8bbdd948e9
Deepika <deepika.m@8kmiles.com>
2017-12-20 16:14:25 +0530

    policy assign

---
+++ PolicyAssignment.ps1
@@ -0,0 +1,3 @@
+﻿$ResourceGroup = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635/resourceGroups/deepikaRG"
+$Policy = Get-AzureRmPolicyDefinition -Name "Require SQL Server version 12.0"
+New-AzureRmPolicyAssignment -Name "Require SQL Server version 12.0 Assignment" -PolicyDefinition $Policy -Scope $ResourceGroup.ResourceId
