ec1db34ec4557734c448796de58a3bdce5357593
Deepika <deepika.m@8kmiles.com>
2017-12-20 18:17:34 +0530

    policy assignment

--- PolicyAssignment.ps1
+++ PolicyAssignment.ps1
@@ -1,3 +1,3 @@
 ﻿$ResourceGroup = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635/resourceGroups/deepikaRG"
-$Policy = Get-AzureRmPolicyDefinition -Name "464dbb85-3d5f-4a1d-bb09-95a9b5dd19cf"
-New-AzureRmPolicyAssignment -Name "Require SQL Server version 12.0 Assignment" -PolicyDefinition $Policy -Scope $ResourceGroup.ResourceId
+$Policy = New-AzureRmPolicyDefinition -Name "464dbb85-3d5f-4a1d-bb09-95a9b5dd19cf" -Policy "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/SQLPolicy.rules.json"
+New-AzureRmPolicyAssignment -Name "Require SQL Server version Assignment" -PolicyDefinition $Policy -Scope $ResourceGroup.ResourceId
