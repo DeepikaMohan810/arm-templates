91ea3468ad367837a62605230c12ad149d6e1b08
Deepika <deepika.m@8kmiles.com>
2017-12-21 12:52:36 +0530

    tagRGand policy

--- PolicyAssignment.ps1
+++ PolicyAssignment.ps1
@@ -1,3 +1,3 @@
-﻿$ResourceGroup = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635/resourceGroups/deepikaRG"
-$Policy = New-AzureRmPolicyDefinition -Name "464dbb85-3d5f-4a1d-bb09-95a9b5dd19cf" -Policy "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/SQLPolicy.rules.json"
-New-AzureRmPolicyAssignment -Name "Require SQL Server version Assignment" -PolicyDefinition $Policy -Scope $ResourceGroup.ResourceId
+﻿$ResourceGroup = Get-AzureRmResourceGroup -Name "deepikaRG" 
+$Policy = New-AzureRmPolicyDefinition -Name "464dbb85-3d5f-4a1d-bb09-95a9b5dd19cf" -Policy "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/SQLPolicy.rules.json" 
+New-AzureRmPolicyAssignment -Name "Require SQL Server version Assignment" -PolicyDefinition $Policy -Scope $ResourceGroup.ResourceId 
