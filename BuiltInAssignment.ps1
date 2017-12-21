43e73850f3950b756c105336c4ccfcf9b867136f
Deepika <deepika.m@8kmiles.com>
2017-12-21 15:24:18 +0530

    commit policy ps

---
+++ BuiltInAssignment.ps1
@@ -0,0 +1,3 @@
+﻿$scope = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635"
+$policy = Get-AzureRmPolicyDefinition -ResourceId "/providers/Microsoft.Authorization/policyDefinitions/c9c29499-c1d1-4195-99bd-2ec9e3a9dc89"
+New-AzureRmPolicyAssignment -Name "Diagnostics Assignment to NSG" -DisplayName "Enable Diagnostics to all NSG" -Scope $scope -PolicyDefinition $policy 
