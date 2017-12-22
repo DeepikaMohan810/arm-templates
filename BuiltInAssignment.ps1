2e08b8860d2b4153d137f513a1535dc9af1cadae
Deepika <deepika.m@8kmiles.com>
2017-12-22 12:38:33 +0530

    rolesps

--- BuiltInAssignment.ps1
+++ BuiltInAssignment.ps1
@@ -1,3 +1,3 @@
-﻿$scope = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635/deepikaRG","/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635/DeepikaTest-RG"
+﻿$scope = "/subscriptions/da9668c3-7699-4079-83d5-1a3f66b2d635"
 $policy = Get-AzureRmPolicyDefinition -ResourceId "/providers/Microsoft.Authorization/policyDefinitions/06a78e20-9358-41c9-923c-fb736d382a4d"
 New-AzureRmPolicyAssignment -Name "Audit Managed Disks" -DisplayName "Create VM using ManagedDisks" -Scope $scope -PolicyDefinition $policy 
---
+++ CustomRBAC.ps1
@@ -0,0 +1,3 @@
+﻿New-AzureRmRoleDefinition -InputFile "https://raw.githubusercontent.com/DeepikaMohan810/arm-templates/arm-stage/RGRoles.json"
+$roleName = Get-AzureRmRoleDefinition -Name "ResourceGroup Reader"
+New-AzureRmRoleAssignment -ObjectId "d485a313-5830-414f-a6a2-778c3e09f1f8" -RoleDefinitionId $roleName.Id
